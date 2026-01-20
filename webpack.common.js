4dde79dc2a329eb7a9d93cc155bae4b76c1b0f7b
James Cao <jcao5360@gmail.com>
2026-01-20 17:07:32 -0500

    implemented multi-page connections

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,34 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+
+const generateHtmlPlugin = (title) => {
+    return new HtmlWebpackPlugin({
+        // not sure what the function of this is
+        title: `${title}`,
+        // the path/URL entered to reach this page: "http://localhost:8080/{filename}"
+        filename: `${title.toLowerCase()}`,
+        // the html path in this project to link to the filename
+        template: `./src/pages/${title.toLowerCase()}/index.html`,
+    });
+}
+const populateHtmlPlugins = (pagesArray) => {
+    const res = [];
+
+    const home = new HtmlWebpackPlugin({
+        title: "Home",
+        template: "./src/pages/home/index.html",
+    });
+    res.push(home);
+
+    pagesArray.forEach(page => {
+        res.push(generateHtmlPlugin(page));
+    })
+    return res;
+}
+const pages = populateHtmlPlugins(["About", "Contact"]);
+
+
 module.exports = {
     entry: "./src/index.js",
     output: {
@@ -10,13 +38,9 @@ module.exports = {
     },
     devtool: "eval-source-map",
     devServer: {
-        watchFiles: ["./src/index.html"],
+        watchFiles: ["./src/pages/home/index.html"],
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-        }),
-    ],
+    plugins: pages,
     module: {
         rules: [
             {
