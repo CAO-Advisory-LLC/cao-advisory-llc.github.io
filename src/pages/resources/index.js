7c2f3fff1bd1313ef8596d4488de261c424bfd5c
James Cao <jcao5360@gmail.com>
2026-02-12 17:45:08 -0500

    added JSON of articles, started adding articles to resources

--- src/pages/resources/index.js
+++ src/pages/resources/index.js
@@ -1,6 +1,33 @@
 import "./style.css";
+import articlesJSON from './assets/articles.json';
 
 function init() {
     console.log("Hello! The resources index.js/main.js has been run!");
+    populateArticles();
 }
 init();
+
+
+
+function populateArticles() {
+    const containerDiv = document.querySelector("#articles .container");
+
+    // apparently, webpack automatically converts imported JSON strings into JS Objects, so no need for JSON.parse()
+    const articlesList = articlesJSON.articles;
+    console.log(articlesList);
+
+    for(let i in articlesList) {
+        const articleDiv = document.createElement("div");
+        articleDiv.classList.add("article");
+        containerDiv.appendChild(articleDiv);
+
+        const title = document.createElement("a");
+        title.textContent = articlesList[i].title;
+        title.setAttribute("href", `${articlesList[i].link}`);
+        articleDiv.appendChild(title);
+
+        const date = document.createElement("p");
+        date.textContent = articlesList[i].date;
+        articleDiv.appendChild(date);
+    }
+}
