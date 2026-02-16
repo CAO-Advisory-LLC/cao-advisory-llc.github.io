b6158139e557ec469858bc1ec5b1bad69a3c07b1
James Cao <jcao5360@gmail.com>
2026-02-16 15:02:12 -0500

    finished basic display for events and mentees

--- src/pages/resources/index.js
+++ src/pages/resources/index.js
@@ -1,9 +1,13 @@
 import "./style.css";
 import articlesJSON from './assets/articles.json';
+import eventsJSON from './assets/events.json';
+import menteesJSON from './assets/mentees.json';
 
 function init() {
     console.log("Hello! The resources index.js/main.js has been run!");
     populateArticles();
+    populateEvents();
+    populateMentees();
 }
 init();
 
@@ -14,7 +18,7 @@ function populateArticles() {
 
     // apparently, webpack automatically converts imported JSON strings into JS Objects, so no need for JSON.parse()
     const articlesList = articlesJSON.articles;
-    console.log(articlesList);
+    // console.log(articlesList);
 
     for(let i in articlesList) {
         const articleDiv = document.createElement("div");
@@ -32,5 +36,82 @@ function populateArticles() {
         date.classList.add("date");
         date.textContent = articlesList[i].date;
         articleDiv.appendChild(date);
+    }
+}
+
+function populateEvents() {
+    const containerDiv = document.querySelector("#events .events-list");
+
+    const eventsList = eventsJSON.events;
+    // console.log(eventsList);
+
+    for(let i in eventsList) {
+        const eventDiv = document.createElement("div");
+        eventDiv.classList.add("event");
+        containerDiv.appendChild(eventDiv);
+
+        const name = document.createElement("p");
+        name.classList.add("name");
+        name.textContent = eventsList[i].name;
+        eventDiv.appendChild(name);
+
+        const date = document.createElement("p");
+        date.classList.add("date");
+        date.textContent = eventsList[i].date;
+        eventDiv.appendChild(date);
+
+        // add details link or record link if they exist
+        if(eventsList[i].details_link !== "") {
+            const details = document.createElement("a");
+            details.classList.add("details");
+            details.textContent = "Details";
+            details.setAttribute("href", `${eventsList[i].details_link}`);
+            details.setAttribute("target", "_blank");
+            eventDiv.appendChild(details);
+        }
+        if(eventsList[i].record_link !== "") {
+            const record = document.createElement("a");
+            record.classList.add("record");
+            record.textContent = "Record";
+            record.setAttribute("href", `${eventsList[i].record_link}`);
+            record.setAttribute("target", "_blank");
+            eventDiv.appendChild(record);
+        }
+        
+    }
+}
+
+function populateMentees() {
+    const containerDiv = document.querySelector("#events .mentees-list");
+
+    const menteesList = menteesJSON.mentees;
+    // console.log(menteesList);
+
+    for(let i in menteesList) {
+        const articleDiv = document.createElement("div");
+        articleDiv.classList.add("mentee");
+        containerDiv.appendChild(articleDiv);
+
+        const course = document.createElement("p");
+        course.classList.add("course");
+        course.textContent = menteesList[i].course;
+        articleDiv.appendChild(course);
+
+        const organization = document.createElement("p");
+        organization.classList.add("organization");
+        organization.textContent = menteesList[i].organization;
+        articleDiv.appendChild(organization);
+
+        const date = document.createElement("p");
+        date.classList.add("date");
+        date.textContent = menteesList[i].date;
+        articleDiv.appendChild(date);
+
+        const link = document.createElement("a");
+        link.classList.add("link");
+        link.textContent = "Link";
+        link.setAttribute("href", `${menteesList[i].link}`);
+        link.setAttribute("target", "_blank");
+        articleDiv.appendChild(link);
     }
 }
